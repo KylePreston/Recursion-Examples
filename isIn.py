50d28dd2eeaf689168b82c02ade4b82c5b9df472
Kyle Preston <kylepreston.122@gmail.com>
2014-03-09 15:37:16 -0700

    Create isIn.py

---
+++ isIn.py
@@ -0,0 +1,13 @@
+def isIn(char, aStr):
+	"""Every time you use is in, this is what's being used. """
+	
+	if len(aStr) == 0:
+		return False
+	elif len(aStr) == 1:
+		return char == aStr
+	elif char == aStr[len(aStr)/2]:
+		return True
+	elif char > aStr[len(aStr)/2]:
+		return isIn(char, aStr[len(aStr)/2:0])
+	elif char < aStr[len(aStr)/2]:
+		return isIn(char, aStr[0:len(aStr)/2])
